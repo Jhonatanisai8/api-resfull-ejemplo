a0839f149bade40fd7789394882736365709d5ae
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-20 16:47:51 -0500

    configuracion inicial del proyecto

---
+++ src/main/java/com/isai/api_resfull_ejemplo/ApiResfullEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.isai.api_resfull_ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResfullEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResfullEjemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/api_resfull_ejemplo/ApiResfullEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.api_resfull_ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResfullEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
