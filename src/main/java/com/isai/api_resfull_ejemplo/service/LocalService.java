d783a6c47e3dc2bd80b1f042b8d22da34d85f008
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-21 15:35:41 -0500

    LocalService: definicion del metodo findLocalByNameWithJPQL

--- src/main/java/com/isai/api_resfull_ejemplo/service/LocalService.java
+++ src/main/java/com/isai/api_resfull_ejemplo/service/LocalService.java
@@ -1,6 +1,7 @@
 package com.isai.api_resfull_ejemplo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.isai.api_resfull_ejemplo.entity.Local;
 
@@ -9,7 +10,9 @@ public interface LocalService {
 
     Local saveLocal(Local local);
 
-    Local updateLocal(Long localID,Local local);
+    Local updateLocal(Long localID, Local local);
 
     void deleteLocal(Long localID);
+
+    Optional<Local> findLocalByNameWithJPQL(String name);
 }
