3f2ef7d45f8abdab99c35795aa941d7643cc2c62
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-21 13:30:57 -0500

    LocalController: implementacion del metodo para guardar un local POST

--- src/main/java/com/isai/api_resfull_ejemplo/controller/LocalController.java
+++ src/main/java/com/isai/api_resfull_ejemplo/controller/LocalController.java
@@ -1,9 +1,10 @@
 package com.isai.api_resfull_ejemplo.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.isai.api_resfull_ejemplo.entity.Local;
@@ -20,4 +21,10 @@ public class LocalController {
     public List<Local> getAllLocals() {
         return serviceImp.findAllLocals();
     }
+
+    @PostMapping("/createLocal")
+    public Local saveLocal(@RequestBody Local local) {
+        return serviceImp.saveLocal(local);
+    }
+
 }
