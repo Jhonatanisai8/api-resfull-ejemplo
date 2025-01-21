544af4ba0ba98026c29cc46e7e49d50ab2227cc9
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-21 15:30:43 -0500

    LocalRepository: definicion del metodo para filtrar un local por su nombre

--- src/main/java/com/isai/api_resfull_ejemplo/repository/LocalRepository.java
+++ src/main/java/com/isai/api_resfull_ejemplo/repository/LocalRepository.java
@@ -1,10 +1,15 @@
 package com.isai.api_resfull_ejemplo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.isai.api_resfull_ejemplo.entity.Local;
 
-public interface LocalRepository 
- extends JpaRepository<Local,Long> {
-    
+public interface LocalRepository
+        extends JpaRepository<Local, Long> {
+
+    @Query("SELECT l FROM Local l WHERE l.name = :name")
+    Optional<Local> findLocalByNameWithJPQL(String name);
 }
