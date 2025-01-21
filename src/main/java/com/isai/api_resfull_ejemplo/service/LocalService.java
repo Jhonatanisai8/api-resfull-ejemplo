26c963039dcd38fb7f1a487c11fc7979fc736229
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-21 11:29:53 -0500

    clase service para local

---
+++ src/main/java/com/isai/api_resfull_ejemplo/service/LocalService.java
@@ -0,0 +1,10 @@
+package com.isai.api_resfull_ejemplo.service;
+
+import java.util.List;
+
+import com.isai.api_resfull_ejemplo.entity.Local;
+
+public interface LocalService {
+    List<Local> findAllLocals();
+
+}
